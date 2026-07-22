fdc09ec58e9c508a6d2fff7ea5d20ee43ee60a3a
Maha-1a <138623711+Maha-1a@users.noreply.github.com>
2026-07-23 03:00:12 +0400

    Update database_helper.dart

--- lib/core/database/database_helper.dart
+++ lib/core/database/database_helper.dart
@@ -1,1 +1,73 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
+class DatabaseHelper {
+
+  static final DatabaseHelper instance = DatabaseHelper._init();
+
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+
+  Future<Database> get database async {
+
+    if (_database != null) return _database!;
+
+    _database = await _initDB('saira_business.db');
+
+    return _database!;
+  }
+
+
+  Future<Database> _initDB(String fileName) async {
+
+    final dbPath = await getDatabasesPath();
+
+    final path = join(dbPath, fileName);
+
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+    );
+  }
+
+
+  Future _createDB(Database db, int version) async {
+
+
+    await db.execute('''
+      CREATE TABLE products (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        category TEXT,
+        price REAL,
+        stock INTEGER
+      )
+    ''');
+
+
+    await db.execute('''
+      CREATE TABLE employees (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        phone TEXT,
+        salary REAL
+      )
+    ''');
+
+
+    await db.execute('''
+      CREATE TABLE customers (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        phone TEXT,
+        due REAL
+      )
+    ''');
+
+  }
+
+}
