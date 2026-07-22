3e606d58cac4aec06821a9914dd38b7ac066aded
Maha-1a <138623711+Maha-1a@users.noreply.github.com>
2026-07-23 02:57:16 +0400

    Update login_screen.dart

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,1 +1,99 @@
+import 'package:flutter/material.dart';
+import '../dashboard/dashboard_screen.dart';
 
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  final userController = TextEditingController();
+  final passController = TextEditingController();
+
+  void login() {
+
+    if(userController.text.isNotEmpty &&
+       passController.text.isNotEmpty){
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const DashboardScreen(),
+        ),
+      );
+
+    } else {
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Enter User ID & Password"),
+        ),
+      );
+
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: [
+
+            const Text(
+              "SAIRA SUPERMARKET",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const Text(
+              "Powered by\nSaira Business System",
+              textAlign: TextAlign.center,
+            ),
+
+            const SizedBox(height:40),
+
+            TextField(
+              controller: userController,
+              decoration: const InputDecoration(
+                labelText: "User ID",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            const SizedBox(height:20),
+
+            TextField(
+              controller: passController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: "Password",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            const SizedBox(height:25),
+
+            ElevatedButton(
+              onPressed: login,
+              child: const Text("LOGIN"),
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+}
