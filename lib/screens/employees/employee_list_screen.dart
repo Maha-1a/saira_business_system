826a59e4983afa9ea9aba49655e6df2c289d3f6b
Maha-1a <138623711+Maha-1a@users.noreply.github.com>
2026-07-23 03:07:33 +0400

    Update employee_list_screen.dart

--- lib/screens/employees/employee_list_screen.dart
+++ lib/screens/employees/employee_list_screen.dart
@@ -1,1 +1,109 @@
+import 'package:flutter/material.dart';
+import '../../core/database/database_helper.dart';
 
+class EmployeeListScreen extends StatefulWidget {
+  const EmployeeListScreen({super.key});
+
+  @override
+  State<EmployeeListScreen> createState() => _EmployeeListScreenState();
+}
+
+class _EmployeeListScreenState extends State<EmployeeListScreen> {
+
+  List employees = [];
+
+
+  Future<void> loadEmployees() async {
+
+    final db = await DatabaseHelper.instance.database;
+
+    final data = await db.query('employees');
+
+    setState(() {
+      employees = data;
+    });
+
+  }
+
+
+  Future<void> deleteEmployee(int id) async {
+
+    final db = await DatabaseHelper.instance.database;
+
+    await db.delete(
+      'employees',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+    loadEmployees();
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    loadEmployees();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      appBar: AppBar(
+        title: const Text("Employees"),
+      ),
+
+
+      body: employees.isEmpty
+
+          ? const Center(
+              child: Text("No Employee Found"),
+            )
+
+          : ListView.builder(
+
+              itemCount: employees.length,
+
+              itemBuilder: (context, index) {
+
+                final employee = employees[index];
+
+                return Card(
+
+                  child: ListTile(
+
+                    title: Text(
+                      employee['name'],
+                    ),
+
+                    subtitle: Text(
+                      "Phone: ${employee['phone']}\n"
+                      "Salary: ${employee['salary']}",
+                    ),
+
+
+                    trailing: IconButton(
+
+                      icon: const Icon(
+                        Icons.delete,
+                      ),
+
+                      onPressed: () {
+
+                        deleteEmployee(
+                          employee['id'],
+                        );
+
+                      },
+                    ),
+                  ),
+                );
+
+              },
+            ),
+    );
+  }
+}
