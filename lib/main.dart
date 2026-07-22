5d9b553a195efb1815a9daf8fb031ec90025c6f8
Maha-1a <138623711+Maha-1a@users.noreply.github.com>
2026-07-23 02:49:47 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+import 'screens/splash/splash_screen.dart';
 
+void main() {
+  runApp(const SairaBusinessSystem());
+}
+
+class SairaBusinessSystem extends StatelessWidget {
+  const SairaBusinessSystem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Saira Business System',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
