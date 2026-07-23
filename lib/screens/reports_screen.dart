644b4fbc518b8e2d85ea3f59431456cfcd7fd4a7
Maha-1a <138623711+Maha-1a@users.noreply.github.com>
2026-07-23 09:06:00 +0400

    Update reports_screen.dart

--- lib/screens/reports_screen.dart
+++ lib/screens/reports_screen.dart
@@ -1,1 +1,47 @@
+import 'package:flutter/material.dart';
 
+class ReportsScreen extends StatelessWidget {
+  const ReportsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Reports"),
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(16),
+        children: const [
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.shopping_cart),
+              title: Text("Sales Report"),
+            ),
+          ),
+          SizedBox(height: 10),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.inventory),
+              title: Text("Product Report"),
+            ),
+          ),
+          SizedBox(height: 10),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.people),
+              title: Text("Employee Report"),
+            ),
+          ),
+          SizedBox(height: 10),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.account_balance_wallet),
+              title: Text("Accounts Report"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
